9654cf758207819d755f4f1fe27d2d9bb370b890
ydb <yindebo@aliyun.com>
2020-04-09 10:03:43 +0800

    Initial commit

---
+++ src/ytt/lc/easy/Solution1.java
@@ -0,0 +1,43 @@
+package ytt.lc.easy;
+
+/**
+ * Description: 两数之和
+ * @author yindb
+ * @date 2020/4/9
+ */
+public class Solution1
+{
+    public static void main(String[] args)
+    {
+        // 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个整数，并返回他们的数组下标。
+        // 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+        // eg1:
+        int[] nums = {-1, 0, -1, 1, 2};
+        int target = 0;
+        int[] result = twoSum(nums, target);
+        System.out.println(result[0] + "," + result[1]);
+    }
+
+    private static int[] twoSum(int[] nums, int target)
+    {
+        /*
+        Solution1：for循环嵌套
+         */
+        // for (int i = 0; i < nums.length; i++)
+        // {
+        //     for (int j = i+1; j < nums.length; j++)
+        //     {
+        //         if (nums[i] + nums[j] == target)
+        //         {
+        //             return new int[]{i, j};
+        //         }
+        //     }
+        // }
+
+        /*
+        Solution2：hashMap存储遍历后计算出的差值，
+         */
+
+        throw new IllegalArgumentException("unsolvable");
+    }
+}
